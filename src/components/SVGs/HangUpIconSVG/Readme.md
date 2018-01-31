cc4d8b2db625a8669b759c776a11690fcd548128
nickguimond <nguimond@serenova.com>
2018-01-31 13:35:12 -0400

    added loading ability to hangup icon

--- src/components/SVGs/HangUpIconSVG/Readme.md
+++ src/components/SVGs/HangUpIconSVG/Readme.md
@@ -2,13 +2,13 @@ HangUpIconSVG example:
 
 ```js
 <div>
-<HangUpIconSVG onClick={()=>console.log('Clicked')} style={{width:'40px', marginRight: '20px'}}/>
-<HangUpIconSVG style={{width:'40px', marginRight: '20px'}}/>
-<HangUpIconSVG style={{width:'40px', marginRight: '20px'}}/>
+<HangUpIconSVG loading onClick={()=>console.log('Clicked')} style={{width:'40px', marginRight: '20px'}}/>
+<HangUpIconSVG loading style={{width:'40px', marginRight: '20px'}}/>
+<HangUpIconSVG loading style={{width:'40px', marginRight: '20px'}}/>
 
-<HangUpIconSVG style={{width:'80px', marginRight: '20px'}}/>
-<HangUpIconSVG style={{width:'80px', marginRight: '20px'}}/>
-<HangUpIconSVG style={{width:'80px', marginRight: '20px'}}/>
-<HangUpIconSVG noBackground style={{width:'30px', marginRight: '20px'}}/>
+<HangUpIconSVG loading style={{width:'80px', marginRight: '20px'}}/>
+<HangUpIconSVG loading style={{width:'80px', marginRight: '20px'}}/>
+<HangUpIconSVG loading style={{width:'80px', marginRight: '20px'}}/>
+<HangUpIconSVG loading noBackground style={{width:'30px', marginRight: '20px'}}/>
 </div>
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -23,16 +23,69 @@ const config = {
     umdNamedDefine: true
   },
   module: {
-    rules: [
+    // rules: [
+    //   {
+    //     test: /(\.jsx|\.js)$/,
+    //     loader: 'babel-loader',
+    //     exclude: /(node_modules|bower_components)/
+    //   },
+    //   {
+    //     test: /(\.jsx|\.js)$/,
+    //     loader: 'eslint-loader',
+    //     exclude: /node_modules/
+    //   }
+    // ]
+    loaders: [
       {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader',
+      }, {
+        test: /\.(eot|svg)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.[ot]tf$/,
+        loader: 'url-loader',
+        query: {
+          limit: 50000,
+          mimetype: 'application/opentype',
+          name: './fonts/[hash].[ext]',
+        },
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loaders: ['file-loader?context=src/images&name=images/[path][name].[ext]', {
+          loader: 'image-webpack-loader',
+          query: {
+            mozjpeg: {
+              progressive: true,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            optipng: {
+              optimizationLevel: 4,
+            },
+            pngquant: {
+              quality: '75-90',
+              speed: 3,
+            },
+          },
+        }],
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.(mp4|webm)$/,
+        loader: 'url-loader?limit=10000',
       }
     ]
   },
