27ab81d5e6b28bf945fa0458ca500bdb61091f5f
rekha42 <48731959+rekha42@users.noreply.github.com>
2019-05-23 09:43:11 -0300

    CXV1-17550- Fixed an issue with displaying year in copyrights section. (#148)
    * CXV1-17550- Fixed an issue with displaying year in copyrights section.
    
    * CXV1-17550- Fixed an issue with displaying year in copyrights section.
    
    * Merge branch 'master' of github.com:rekha42/cx-ui-components
    
    * CXV1-17550- Fixed an issue with displaying year in copyrights section.
    
    * Merge branch 'master' of github.com:rekha42/cx-ui-components
    
    * CXV1-17550 Added logs and legalcopyright components to the config-ui part
    
    * Merge pull request #1 from rekha42/CXV1-17550
    
    CXV1-17550 Added logs and legalcopyright components to the config-ui …
    
    * CXV1-17550 Added logs and legalcopyright components to the config-ui part
    
    * CXV1-17550- Fixed an issue with displaying year in copyrights section.
    
    * Merge branch 'master' of github.com:rekha42/cx-ui-components
    
    * CXV1-17550 Added logs and legalcopyright components to the config-ui part
    
    * CXV1-17550- Fixed an issue with displaying year in copyrights section.
    
    * Merge branch 'master' of github.com:rekha42/cx-ui-components
    
    * CXV1-17550 Added logs and legalcopyright components to the config-ui part
    
    * CXV1-17550- Fixed an issue with displaying year in copyrights section.
    
    * Merge branch 'master' of github.com:rekha42/cx-ui-components
    
    * resolved merge conflict.
    
    * resolved merge conflict.
    
    * Merge branch 'master' of github.com:rekha42/cx-ui-components

--- src/components/ConfigUi/LegalCopyright/message.js
+++ src/components/ConfigUi/LegalCopyright/message.js
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   copyright: {
     id: 'app.component.Login.copyright',
-    defaultMessage: 'Copyright © 2015-{year} Serenova, LLC ("Serenova"). All rights reserved.'
+    defaultMessage: 'Copyright © 2015-{2019} Serenova, LLC ("Serenova"). All rights reserved.'
   },
   legal: {
     id: 'app.component.Login.legal',
